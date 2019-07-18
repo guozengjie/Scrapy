cd84acf93bc4b02096b4588d73a1d6e3a81536f6
guozengjie <guozengjie110@foxmail.com>
2019-07-18 20:11:23 +0800

    第一次提交

--- Demo02/spiders/biqukan.py
+++ Demo02/spiders/biqukan.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.selector import Selector
 
 class BiqukanSpider(scrapy.Spider):
     name = 'biqukan'
@@ -8,4 +8,10 @@ class BiqukanSpider(scrapy.Spider):
     start_urls = ['https://www.biqukan.com/3_3037/']
 
     def parse(self, response):
-        pass
+        print(response)
+        # 创建一个选择器的实例
+        select=Selector(response)
+        dd_list=select.css("dd:nth-child(n+15)")
+        for dd in dd_list:
+            pass
+
