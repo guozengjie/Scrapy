0c9dcb98a17e9c1e8b0238de91c45637bdea22ec
guozengjie <guozengjie110@foxmail.com>
2019-07-23 11:13:59 +0800

    第一次提交

--- Demo02/spiders/biqukan.py
+++ Demo02/spiders/biqukan.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from urllib import parse
+
 import scrapy
+from scrapy import Request
 from scrapy.selector import Selector
 
 from Demo02.items import Demo02Item
@@ -11,7 +14,6 @@ class BiqukanSpider(scrapy.Spider):
     start_urls = ['https://www.biqukan.com/3_3037/']
 
     def parse(self, response):
-        print(response)
         # 创建一个选择器的实例
         select = Selector(response)
         dd_list = select.css("dd:nth-child(n+15)")
@@ -19,3 +21,8 @@ class BiqukanSpider(scrapy.Spider):
             item = Demo02Item()
             item['title'] = dd.css('a::text').get()
             item['href'] = dd.css('a::attr(href)').get()
+            request = Request(parse.urljoin(response.url, item['href']), callback=self.parse_body)
+            yield request
+
+    def parse_body(self, response):
+        print('---------------------', response)
